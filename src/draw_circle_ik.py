872ae5900af9f9f6f0d5b29745c69602d6bfc0de
Rikki <rikkiirwin@gmail.com>
2015-10-13 22:44:46 -0500

    Working circle

--- src/draw_circle_ik.py
+++ src/draw_circle_ik.py
@@ -2,24 +2,23 @@
 
 import rospy
 import roslib
-import math import sqrt, pi, acos, atan2, cos
+from math import sqrt, pi, acos, atan2, cos, sin
 from sensor_msgs.msg import JointState
 
 def draw_circle_ik():
 
-	pub = rospy.Publisher('joint_states',Twist, queue_size = 10)
-	rospy.init_node('ik', anonymous=True)
-	rate = rospy.Rate(50)
-	joint_state = JointState()
-	joint_state.header = Header()
-	joint_state.header.stamp = rospy.Time.now()
-	joint_state.name = ['th1','th2']
-	joint_state.position = [0,0]
-	joint_state.velocity = []
-	joint_state.effort = []
+    pub = rospy.Publisher('joint_states', JointState, queue_size = 10)    
+    rospy.init_node('ik', anonymous=True)
+    rate = rospy.Rate(50)
+    joint_state = JointState()
+    joint_state.header.stamp = rospy.Time.now()
+    joint_state.name = ['joint1','joint2']
+    joint_state.position = [0,0]
+    joint_state.velocity = []
+    joint_state.effort = []
 	
 
-	while not rospy.is_shutdown():
+    while not rospy.is_shutdown():
 
         # Calculate desired x and y positions
         t = rospy.get_time()
@@ -27,17 +26,21 @@ def draw_circle_ik():
         y = .5*sin(2*pi*t/5)
         
         # Inverse kinematics
+        l1 = 1
+        l2 = 1
         B = sqrt(x**2+y**2)
+ #       rospy.loginfo(B)
         q1 = atan2(y,x)
-        q2 = acos((B**2+l1**2-l2**2/(2*B*l1))
+        q2 = acos((B**2+l1**2-l2**2)/(2*B*l1))
         th1 = q1-q2
         q3 = acos((l1**2+l2**2-B**2)/(2*l1*l2))
         th2 = pi-q3
 		
-		# Publish angles
-		joint_state.position = [th1,th2]
-		rospy.loginfo(angle_cmd)
-		pub.publish(angle_cmd)
+        # Publish angles
+        joint_state.header.stamp = rospy.Time.now()
+	joint_state.position = [th1,th2]
+#	rospy.loginfo(joint_state)
+	pub.publish(joint_state)
         rate.sleep()
                                                                                                                          
 if __name__ == "__main__":
